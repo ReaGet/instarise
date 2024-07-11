2fc63593b2e9238487a6846984bd72b583f5bbf8
Rifat Mukhametzyanov <rifat@avtoalfa.com>
2024-07-11 16:55:20 +0300

    chore: add dropdown

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,19 @@
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+
 const Header = () => {
   return (
     <header className='w-full h-20 border-b px-6'>
-      header
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </header>
   )
 }
