b357a17bdbac1ca8b7696883d759f9839867a982
Rifat Mukhametzyanov <rifat@avtoalfa.com>
2024-07-11 15:32:36 +0300

    feat: setup shadecn. add error page

--- src/app/error-page.tsx
+++ src/app/error-page.tsx
@@ -1,4 +1,6 @@
-import { useRouteError } from 'react-router-dom'
+import { Link, useRouteError } from 'react-router-dom'
+import { Button } from '../components/ui/button';
+import { DASHBOARD } from '../consts';
 
 interface IError {
   statusText: string;
@@ -9,12 +11,16 @@ const ErrorPage = () => {
   const error = useRouteError() as IError;
   console.log(error)
   return (
-    <div className='flex flex-col items-center justify-center gap-12 w-screen h-screen px-6 text-3xl'>
-      <h1 className='text-7xl font-bold'>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p className='text-gray-500'>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div className='flex flex-col items-center justify-center gap-12 w-screen h-screen px-6 text-primary'>
+      <h1 className='text-5xl font-bold'>Oops!</h1>
+      <div className='flex items-center gap-6'>
+        <span>Sorry, an unexpected error has occurred.</span>
+        <div className='bg-secondary w-[1px] h-12'></div>
+        <span>{error.statusText || error.message}</span>
+      </div>
+      <Button asChild size={'lg'}>
+        <Link to={DASHBOARD}>Go Home</Link>
+      </Button>
     </div>
   )
 }
